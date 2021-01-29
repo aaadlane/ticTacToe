324adbb4bbb4135c248ad08cd36abd92a420dc89
Jozuah <galbas.joshua@gmail.com>
2021-01-29 11:52:48 +0100

    ajout d' une fonction choice()

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-// test script
-qsdd
+//let grid_list = [[null,null,null],[null],[]]
+
+let caseGame = document.querySelectorAll("#grid-play div")
+console.log(caseGame)
+
+caseGame.forEach(function(choix){
+    choix.addEventListener("click", choicePlace)
+})
+
+function choicePlace(){
+    console.log("YO!")  
+}
