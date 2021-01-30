8f7705a1c62425a36342e1d7f12918ac457ba844
Jozuah <galbas.joshua@gmail.com>
2021-01-30 23:07:24 +0100

    ajout d'une fonction qui store les noms des joueurs

--- script.js
+++ script.js
@@ -1,51 +1,13 @@
-// //get data from input
-
-// var radios = document.getElementsByName('rbtn');
-
-// for (var i = 0, length = radios.length; i < length; i++) {
-//     if (radios[i].checked) {
-//         alert(radios[i].value);
-//         break;
-//     }
-// }
-
-
-// let caseGame = document.querySelectorAll("#grid-play div")
-// console.log(caseGame)
-
-// let x = null
-
-
-// caseGame.forEach(function(choix){
-//     x = choix
-//     //permet 1 seul click sur la div
-//     choix.addEventListener("click", choicePlace,{once:true})
-// })
-
-
-// // Fonction qui retourne l'index de la case selectionné
-// function choicePlace(event){
-
-//     let index;
-//     caseGame.forEach((element,i)=> {
-//         if (event.target === element){
-//             console.log(element,i)
-//             index=i
-
-//         }
-//     })
-// }
-
-
-
-//code david
-
-
 let current_player = "X"
 let caseGame = document.querySelectorAll("#grid-play .cell")
+//let player1 = document.getElementById("player1").value
+let player2 = ""
+var player1 = ""
 
 
 caseGame.forEach(function(choix){
+    player1 = localStorage.player1
+    player2 = localStorage.player2
 
     let nextGame = 0
     //permet 1 seul click sur la div avec once:true
@@ -54,6 +16,7 @@ caseGame.forEach(function(choix){
         event.target.innerText = current_player
        
         nextGame = winCondition(current_player)
+
         if (nextGame === true){
             window.open("./lastName.html", "_self")
         }
@@ -64,6 +27,8 @@ caseGame.forEach(function(choix){
         else {
             current_player = "X"
         }
+        console.log("joueur 1 :", player1)
+        console.log("joueur 2 :", player2)
     },{once:true})
 
 
@@ -78,27 +43,55 @@ function winCondition(icon){
     }
     else if (tictactoeGrid[3] === tictactoeGrid[4] && tictactoeGrid[3]=== tictactoeGrid[5] && tictactoeGrid[3] != ""){
         console.log(icon,"Partie gagné !")
+        return true
     }
     else if (tictactoeGrid[6] === tictactoeGrid[7] && tictactoeGrid[6]=== tictactoeGrid[8] && tictactoeGrid[6] != ""){
         console.log(icon,"Partie gagné !")
+        return true
     }
     else if (tictactoeGrid[0] === tictactoeGrid[3] && tictactoeGrid[0]=== tictactoeGrid[6] && tictactoeGrid[0] != ""){
         console.log(icon,"Partie gagné !")
+        return true
     }
     else if (tictactoeGrid[1] === tictactoeGrid[4] && tictactoeGrid[1]=== tictactoeGrid[7] && tictactoeGrid[1] != ""){
         console.log(icon,"Partie gagné !")
+        return true
     }
     else if (tictactoeGrid[2] === tictactoeGrid[5] && tictactoeGrid[2]=== tictactoeGrid[8] && tictactoeGrid[2] != ""){
         console.log(icon,"Partie gagné !")
+        return true
     }
     else if (tictactoeGrid[0] === tictactoeGrid[4] && tictactoeGrid[0]=== tictactoeGrid[8] && tictactoeGrid[0] != ""){
         console.log(icon,"Partie gagné !")
+        return true
     }
     else if (tictactoeGrid[2] === tictactoeGrid[4] && tictactoeGrid[2]=== tictactoeGrid[6] && tictactoeGrid[2] != ""){
         console.log(icon,"Partie gagné !")
+        return true
     }
     else if (tictactoeGrid[0] != "" && tictactoeGrid[1] != "" && tictactoeGrid[2] != "" && tictactoeGrid[3] != "" && tictactoeGrid[4] != "" && tictactoeGrid[5] != "" && tictactoeGrid[6] != "" && tictactoeGrid[7] != "" && tictactoeGrid[8] != "" ){
         console.log("Draw, aucun gagnant")
+        return true
     }
 }
-//event.target.innerText = current_player
+
+
+// On attribue un nom a chaque joueur
+document.querySelector(".start").addEventListener("click",function(){
+    console.log("blablabla")
+    //player1=document.getElementById("player1").value
+    localStorage.player1 = document.getElementById("player1").value
+    console.log("joueur test :", localStorage.player1)
+    localStorage.player2 = document.getElementById("player2").value
+
+    window.open("./play.html","_self")
+
+})
+
+// function myFunction() {
+//     var player1 = document.getElementById("player1").value;
+//     var player2 = document.getElementById("player2").value;
+//     submitOK = "true";
+// }  
+
+
