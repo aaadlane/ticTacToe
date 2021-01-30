0b6a8fba839a2475cd8d60ee31a82c38751399e0
Jozuah <galbas.joshua@gmail.com>
2021-01-31 00:03:39 +0100

    ajout d'une fonction qui prend en charge le choix d'icone du joueur et qui l'utilise dans la page de jeu

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-let current_player = "X"
 let caseGame = document.querySelectorAll("#grid-play .cell")
-//let player1 = document.getElementById("player1").value
+
 let player2 = ""
-var player1 = ""
+let player1 = ""
+
+let choix1 = ""
+let current_player = ""
 
 
 caseGame.forEach(function(choix){
+
+    // Les noms des players peuvent être utilisé grâce au local storage
     player1 = localStorage.player1
     player2 = localStorage.player2
 
+    //le current player a son icone définie à la page précédente
+    current_player = localStorage.choix1
+
+    //Initialisation de la variable de fin
     let nextGame = 0
     //permet 1 seul click sur la div avec once:true
     choix.addEventListener("click", function(){
         //Au clic, je met X ou O dans la case correspondante
         event.target.innerText = current_player
-       
+        
+        // La fonction retourne true si il y a victoire ou égalité
         nextGame = winCondition(current_player)
-
+        
+        //si la condition de victoire est réalisé, on bascule sur une autre page
         if (nextGame === true){
             window.open("./lastName.html", "_self")
         }
 
+        //cchangement de joueur si la winCondition n'est pas réalisée
         if (current_player==="X"){
             current_player = "O"
         }  
@@ -75,23 +86,31 @@ function winCondition(icon){
     }
 }
 
-
-// On attribue un nom a chaque joueur
 document.querySelector(".start").addEventListener("click",function(){
-    console.log("blablabla")
-    //player1=document.getElementById("player1").value
+
     localStorage.player1 = document.getElementById("player1").value
     console.log("joueur test :", localStorage.player1)
     localStorage.player2 = document.getElementById("player2").value
 
-    window.open("./play.html","_self")
+    choix1 = document.querySelector('input[name="player1icon"]:checked').value;
+    var choix2 = document.querySelector('input[name="player2icon"]:checked').value;
+    console.log("Le choix 1 est :",choix1)
+    console.log("Le choix 2 est :",choix2)
+    
+    //on reste sur la page tant que les 2 icones sont identiques
+    if (choix1 === choix2){
+        alert("Deux joueurs ne peuvent pas avoir la même icone")
+    }
+
+    //je store le choix du joueur 1 pour utliser sont icone en premier dans la page de jeu
+    if (choix1 != choix2){
+        localStorage.choix1 = choix1
+        window.open("./play.html","_self")
+    }
+
+    
 
 })
 
-// function myFunction() {
-//     var player1 = document.getElementById("player1").value;
-//     var player2 = document.getElementById("player2").value;
-//     submitOK = "true";
-// }  
 
 
